ec30a6f9de15c6ff68f60861310ccbb794d3a898
Juan Manuel Noli <juanmanuelnoli03@gmail.com>
2024-11-01 19:08:28 -0300

    Test

---
+++ src/test/java/com/bbva/tp_integrador_java/TpIntegradorJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbva.tp_integrador_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpIntegradorJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
